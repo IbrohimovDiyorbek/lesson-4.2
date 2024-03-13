0803aff3f70629a697acb8cb12089f5bcbf0bf88
IbrohimovDiyorbek <diyorbekibrohimov31@gmail.com>
2024-03-13 10:45:23 +0500

    it is new homawork

---
+++ mein.js
@@ -0,0 +1,5 @@
+let a = String(prompt("String typedagi malumot kiriting"));
+let b = Number(prompt("Number typedagi malumot kiriting"));
+let c = Boolean(prompt("Boolean typedagi malumot kiriting"));
+alert("Kiritgan ma'lumotlaringizni tasdiqlaysizmi")
+alert("Siz kiritgan birinchi ma'lumot turi" + typeof(a) + "\n" + "Siz kiritgan ikkinchi ma'lumot turi" + typeof(b) + "\n" + "Siz kiritgan uchinchi ma'lumot turi" + typeof(c))
